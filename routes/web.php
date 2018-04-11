5f7e08957fea15add94c058d5bd5317adb22fd58
Eslam-Mamdouh <eslam.mamdouh@abmegypt.com>
2018-04-11 19:50:19 +0200

    Admin Area Design

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('admin.admin');
+});
+
+Route::get('/orders', function () {
+    return view('admin.orders');
+});
